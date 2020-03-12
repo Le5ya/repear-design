27efadaaa93acb6800a7ca565fc39841eb9b8828
Leya Leya <liliatolstyh@yandex.ru>
2020-03-12 19:05:41 +0300

    modified main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-/*document.addEventListener('DOMContentLoaded', function(event){
-	const modal = document.querySelector('.modal');
-	const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-	const closeBtn = document.querySelector('.modal__close');
-	const switchModal = () => {
-			modal.classList.toggle('modal--visible');
-	  }
-	modalBtn.forEach(element => {
-   element.addEventListener('click', switchModal);
-	});
+document.addEventListener('DOMContentLoaded', function(event){
+	// const modal = document.querySelector('.modal');
+	// const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+	// const closeBtn = document.querySelector('.modal__close');
+	// const switchModal = () => {
+	// 		modal.classList.toggle('modal--visible');
+	//   }
+	// modalBtn.forEach(element => {
+ //   element.addEventListener('click', switchModal);
+	// });
 
-  closeBtn.addEventListener('click', switchModal);
+ //  closeBtn.addEventListener('click', switchModal);
 
-  document.addEventListener('keydown', function(event){
-  	if (event.code == 'Escape') {
-  		switchModal();
-  	}
-  });
+ //  document.addEventListener('keydown', function(event){
+ //  	if (event.code == 'Escape') {
+ //  		switchModal();
+ //  	}
+ //  });
+  
 
-});*/
+
+ });
 
 $(document).ready(function () {
 	var modal = $('.modal'),
@@ -30,27 +32,25 @@ $(document).ready(function () {
 	closeBtn.on('click', function() {
   modal.toggleClass('modal--visible')
 	});
-});
+    // Вверх к началу jQ
+	var backTopBtn = $('.button__back-top'),
+		sitePage = $('body,html'); 
+    
+	 backTopBtn.on('click', function() {  
+	 sitePage.animate({scrollTop:0},500);    
+	  });  
+ });
+
+
 
 // вверх к началу JS
-const backTopBtn = document.querySelector('.button__back-top');
-const up =() => {  
- let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);  
-if(top > 0) {  
-  window.scrollBy(0,((top+100)/-10));  
-  t = setTimeout('up()',20);  
-} else clearTimeout(t);  
-return false;  
-} 
-backTopBtn.addEventListener('click', up);
-
-/*$(document).ready(function() {
-var backTopBtn = $('.back-top'),
-		sitePage = $('body'); 
-    
- backTopBtn.on('click', function() {  
- sitePage.animate({scrollTop:0},500);    
-  });  
-   
-});   
-*/
+	// const backTopBtn = document.querySelector('.button__back-top');
+	// const up =() => {  
+	//  let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);  
+	// if(top > 0) {  
+	//   window.scrollBy(0,((top+100)/-10));  
+	//   t = setTimeout('up()',20);  
+	// } else clearTimeout(t);  
+	// return false;  
+	// } 
+	// backTopBtn.addEventListener('click', up);
